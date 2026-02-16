1601e01c8f7a21aca8b203a3c1e99aedaa728e46
Flexer144 <lopatanikita61@gmail.com>
2026-02-16 13:18:45 +0300

    Добавлены фильтрация видео

--- VibeTube/src/pages/Home/Home.tsx
+++ VibeTube/src/pages/Home/Home.tsx
@@ -10,43 +10,89 @@ export default function Home() {
   const { user, profile, loading } = useAuth();
   const [videos, setVideos] = useState<any[]>([]);
   const [loadingVideo, setLoadingVideo] = useState(true);
+  const [genres, setGenres] = useState<any[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<number | null>(null);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    fetchVideos();
-  }, []);
+  const fetchVideos = async (genreId?: number) => {
+    setLoadingVideo(true);
 
-  if (loading) return <p>Загрузка...</p>;
-  if (!user) return <p>Не авторизован</p>;
-
-  const fetchVideos = async () => {
-    const { data, error } = await supabase
+    let query = supabase
       .from("videos")
       .select(`
         *,
         profiles(username),
         video_genres(
-          genres(name,id)
+          genres(*)
         )
       `)
       .order("created_at", { ascending: false });
-      
-    if (error) {
-      console.error(error);
-    } else {
-      setVideos(data || []);
+
+    if (genreId) {
+      query = supabase
+        .from("videos")
+        .select(`
+          *,
+          profiles(username),
+          video_genres!inner(
+            genre_id,
+            genres(*)
+          )
+        `)
+        .eq("video_genres.genre_id", genreId)
+        .order("created_at", { ascending: false });
     }
 
+    const { data, error } = await query;
+
+    if (error) console.error(error);
+
+    setVideos(data || []);
     setLoadingVideo(false);
   };
 
-  if (loadingVideo) return <p>Загрузка...</p>;
+
+
+  const fetchGenres = async () => {
+    const { data } = await supabase.from("genres").select("*");
+    setGenres(data || []);
+  };
+
+  useEffect(() => {
+    fetchVideos();
+    fetchGenres();
+  }, []);
+
+  const handleGenreChange = (id: number | null) => {
+    setSelectedGenre(id);
+    fetchVideos(id ?? undefined);
+  };
+
+  if (loading) return <p>Загрузка...</p>;
+  if (!user) return <p>Не авторизован</p>;
 
   return (
     <>
       <h2>Привет, {profile?.username} 👋</h2>
       <button onClick={logoutUser}>Выйти</button>
       <button onClick={() => navigate("/upload")}>Загрузить видео</button>
+
+      <button
+        className={selectedGenre === null ? "activeGenre" : ""}
+        onClick={() => handleGenreChange(null)}
+      >
+        Все
+      </button>
+
+      {genres.map((g) => (
+        <button
+          key={g.id}
+          className={selectedGenre === g.id ? "activeGenre" : ""}
+          onClick={() => handleGenreChange(g.id)}
+        >
+          {g.name}
+        </button>
+      ))}
 
       <div className="home-page">
       <h2>Видео</h2>
